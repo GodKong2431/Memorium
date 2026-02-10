8f3d48b992d310a186851cc02ca7ff974c793264
ForestPool <zxpqzx123456@gmail.com>
2026-02-10 16:35:24 +0900

    [Add] 플레이어 스탯 기능 구현
    플레이어 스탯 기능 구현을 하였습니다
    
    메인기능
    - MVP패턴을 이용하였습니다
    - 업그레이드 버튼을 누르면 플레이어의 스탯이 늘어납니다

--- Assets/Scripts/Player/PlayerStat.cs
+++ Assets/Scripts/Player/PlayerStat.cs
@@ -1,67 +1,183 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
+
+public enum StatType
+{
+    HP,HPRegen,
+    ATK, ATKSpeed,
+    DEF, MagicDEF,
+    MP, MPRegen,
+    CritChance, CritMult,
+    SkillDamage, CoolDown,
+    MoveSpeed,
+    ExpGain, GoldGain
+}
 
 public class PlayerStat : MonoBehaviour
 {
+    // 기본 스탯
     [Header("Health")]
     [Tooltip("플레이어의 체력")]
-    [SerializeField] private float healthPoint;
+    [SerializeField] private float baseHealthPoint;
     [Tooltip("플레이어의 체력 재생")]
-    [SerializeField] private float hpRegeneration;
+    [SerializeField] private float baseHpRegeneration;
 
     [Header("Attack")]
     [Tooltip("플레이어의 공격력")]
-    [SerializeField] private float attack;
+    [SerializeField] private float baseAttack;
     [Tooltip("플레이어의 공격속도")]
-    [SerializeField] private float attackSpeed;
+    [SerializeField] private float baseAttackSpeed;
 
     [Header("Resistance")]
     [Tooltip("플레이어의 물리저항력")]
-    [SerializeField] private float defense;
+    [SerializeField] private float baseDefense;
     [Tooltip("플레이어의 마법저항력")]
-    [SerializeField] private float magicResistance;
+    [SerializeField] private float baseMagicResistance;
 
     [Header("Mana")]
     [Tooltip("플레이어의 마나")]
-    [SerializeField] private float mana;
+    [SerializeField] private float baseMana;
     [Tooltip("플레이어의 마나재생")]
-    [SerializeField] private float manaRegeneration;
+    [SerializeField] private float baseManaRegeneration;
 
     [Header("Critical")]
     [Tooltip("플레이어의 치명타 확률")]
-    [SerializeField] private float criticalChance;
+    [SerializeField] private float baseCriticalChance;
     [Tooltip("플레이어의 치명타 배율")]
-    [SerializeField] private float criticalMultiplier;
+    [SerializeField] private float baseCriticalMultiplier;
 
     [Header("Skill")]
     [Tooltip("플레이어의 스킬데미지")]
-    [SerializeField] private float skillDamage;
+    [SerializeField] private float baseSkillDamage;
     [Tooltip("플레이어의 쿨타임")]
-    [SerializeField] private float coolTime;
+    [SerializeField] private float baseCoolDown;
 
     [Header("Speed")]
     [Tooltip("플레이어의 이동속도")]
-    [SerializeField] private float moveSpeed;
+    [SerializeField] private float baseMoveSpeed;
 
     [Header("Gain")]
     [Tooltip("플레이어의 골드 획득량")]
-    [SerializeField] private float goldGain;
+    [SerializeField] private float baseGoldGain;
     [Tooltip("플레이어의 경험치 획득량")]
-    [SerializeField] private float expGain;
+    [SerializeField] private float baseExpGain;
 
-    // 다른곳에서 가져가기
-    public float getHealth { get { return healthPoint; } }
-    public float getHpRegeneration { get { return hpRegeneration; } }
-    public float getAttack {  get { return attack; } }
-    public float getAttackSpeed { get { return attackSpeed; } }
-    public float getDefense { get { return defense; } }
-    public float getMagicResistance { get { return magicResistance; } }
-    public float getMana { get { return mana; } }
-    public float getManaRegeneration { get { return manaRegeneration;} }
-    public float getCriticalChance { get { return criticalChance; } }
-    public float getCriticalMultiplier { get { return criticalMultiplier; } }
-    public float getSkillDamage { get { return skillDamage; } }
-    public float getCoolTime { get { return coolTime; } }
-    public float getMoveSpeed { get { return moveSpeed; } }
-    public float getExpGain { get { return expGain; } }
-    public float getGoldGain { get { return goldGain; } }
+    // 이벤트
+    public event Action<StatType,float> StatChanged;
+
+    public float HealthPoint { get { return baseHealthPoint; } }
+    public float HpRegeneration { get { return baseHpRegeneration; } }
+    public float Attack {  get { return baseAttack; } }
+    public float AttackSpeed { get { return baseAttackSpeed; } }
+    public float Defense { get { return baseDefense; } }
+    public float MagicResistance { get { return baseMagicResistance; } }
+    public float Mana { get { return baseMana; } }
+    public float ManaRegeneration { get { return baseManaRegeneration;} }
+    public float CriticalChance { get { return baseCriticalChance; } }
+    public float CriticalMultiplier { get { return baseCriticalMultiplier; } }
+    public float SkillDamage { get { return baseSkillDamage; } }
+    public float CoolDown { get { return baseCoolDown; } }
+    public float MoveSpeed { get { return baseMoveSpeed; } }
+    public float ExpGain { get { return baseExpGain; } }
+    public float GoldGain { get { return baseGoldGain; } }
+
+    // 스탯 가져오기
+    public float GetStat(StatType type)
+    {
+        return type switch
+        {
+            StatType.HP => baseHealthPoint,
+            StatType.HPRegen => baseHpRegeneration,
+            StatType.ATK => baseAttack,
+            StatType.ATKSpeed => baseAttackSpeed,
+            StatType.DEF => baseDefense,
+            StatType.MagicDEF => baseMagicResistance,
+            StatType.MP => baseMana,
+            StatType.MPRegen => baseManaRegeneration,
+            StatType.CritChance => baseCriticalChance,
+            StatType.CritMult => baseCriticalMultiplier,
+            StatType.SkillDamage => baseSkillDamage,
+            StatType.CoolDown => baseCoolDown,
+            StatType.MoveSpeed => baseMoveSpeed,
+            StatType.ExpGain => baseExpGain,
+            StatType.GoldGain => baseGoldGain,
+            _ => 0f
+        };
+    }
+
+    // 스탯 설정
+    public void SetStat(StatType statType, float value)
+    {
+        switch (statType)
+        {
+            case StatType.HP:
+                Set(ref baseHealthPoint, value, statType);
+                break;
+            case StatType.HPRegen:
+                Set(ref baseHpRegeneration, value, statType);
+                break;
+            case StatType.ATK:
+                Set(ref baseAttack, value, statType);
+                break;
+            case StatType.ATKSpeed:
+                Set(ref baseAttackSpeed, value, statType);
+                break;
+            case StatType.DEF:
+                Set(ref baseDefense, value, statType);
+                break;
+            case StatType.MagicDEF:
+                Set(ref baseMagicResistance, value, statType);
+                break;
+            case StatType.MP:
+                Set(ref baseMana, value, statType);
+                break;
+            case StatType.MPRegen:
+                Set(ref baseManaRegeneration, value, statType);
+                break;
+            case StatType.CritChance:
+                Set(ref baseCriticalChance, value, statType);
+                break;
+            case StatType.CritMult:
+                Set(ref baseCriticalMultiplier, value, statType);
+                break;
+            case StatType.SkillDamage:
+                Set(ref baseSkillDamage, value, statType);
+                break;
+            case StatType.CoolDown:
+                Set(ref baseCoolDown, value, statType);
+                break;
+            case StatType.MoveSpeed:
+                Set(ref baseMoveSpeed, value, statType);
+                break;
+            case StatType.ExpGain:
+                Set(ref baseExpGain, value, statType);
+                break;
+            case StatType.GoldGain:
+                Set(ref baseGoldGain, value, statType);
+                break;
+            default:
+                Debug.Log($"{statType.ToString()}에 해당하는 스탯이 없습니다");
+                break;
+        }
+    }
+
+    // 값 변동
+    private void Set(ref float target, float value, StatType type)
+    {
+        target = value;
+        StatChanged?.Invoke(type, target);
+    }
+    
+    // 더하기
+    public void AddStat(float value, StatType type)
+    {
+        SetStat(type, GetStat(type) + value);
+    }
+
+    // 곱하기
+    public void MulStat(float value, StatType type)
+    {
+        SetStat(type, GetStat(type) * value);
+    }
 }
