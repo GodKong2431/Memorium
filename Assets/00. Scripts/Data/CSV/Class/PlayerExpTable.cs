e27402c61839db9787276274c40a5b3be2542aa6
GodKong2431 <kizna10231@gmail.com>
2026-02-11 10:57:55 +0900

    [Add] CSV 파서 제작 및 어드레서블 자동화

---
+++ Assets/00. Scripts/Data/CSV/Class/PlayerExpTable.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class PlayerExpTable
+{
+    public int ID;
+    public int playerLevel;
+    public BigDouble reqExp;
+}
