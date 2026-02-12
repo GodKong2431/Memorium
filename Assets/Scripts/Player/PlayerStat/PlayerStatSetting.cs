967f14a253a19cb25716205e6a80db19a3f7b1ea
ForestPool <zxpqzx123456@gmail.com>
2026-02-12 12:08:10 +0900

    [Add] 캐릭터의 스탯 기능
    스탯 적용할 SO와 스탯 설정하는 기능 추가

---
+++ Assets/Scripts/Player/PlayerStat/PlayerStatSetting.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "PlayerStat", menuName = "Memorium/Player Stat Setting")]
+public class PlayerStatSetting : ScriptableObject
+{
+    public PlayerBaseStatData stat;
+}
