e3d061d3651a46fc674f642f482c35b8b1793814
ForestPool <zxpqzx123456@gmail.com>
2026-02-13 17:53:50 +0900

    [Delete] 필요없어진 파일 삭제

--- Assets/00. Scripts/Player/PlayerStat/Data/ClassType.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-
-public enum classType
-{
-    Assassin,
-    Warrior,
-    Wizard
-}
--- Assets/00. Scripts/Player/PlayerStat/PlayerStatSetting.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-
-[CreateAssetMenu(fileName = "PlayerStat", menuName = "Memorium/Player Stat Setting")]
-public class PlayerStatSetting : ScriptableObject
-{
-    public PlayerBaseStatData stat;
-}
