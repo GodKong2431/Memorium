69604ab5170c7c224b86e7d6c62f956eb2007500
ForestPool <zxpqzx123456@gmail.com>
2026-02-20 16:26:02 +0900

    [Add] 플레이어 장비 장착 스탯 상승 기능 추가 및 테스트

--- Assets/00. Scripts/Player/PlayerStat/CharacterStatManager.cs
+++ Assets/00. Scripts/Player/PlayerStat/CharacterStatManager.cs
@@ -4,12 +4,15 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
-public class CharacterStatManager : MonoBehaviour
+public class CharacterStatManager : Singleton<CharacterStatManager>
 {
     // 키들
     [SerializeField] private int characterBaseKey;
     [SerializeField] private int characterTableKey;
     [SerializeField] private int level;
+
+    [SerializeField] TestSavePlayerEquipmentData testSaveData;
+    [SerializeField] EquipmentHandler equipmentHandler;
 
     [SerializeField] private TraitManager traitManager;
 
@@ -39,12 +42,33 @@ public class CharacterStatManager : MonoBehaviour
     [SerializeField] private PlayerTrait coolDownTrait;
     [SerializeField] private PlayerTrait damageMultTrait;
 
+    [SerializeField] private float finalHP;
+    [SerializeField] private float finalHPRegen;
+    [SerializeField] private float finalMP;
+    [SerializeField] private float finalMPRegen;
+    [SerializeField] private float finalATK;
+    [SerializeField] private float finalATKSpeed;
+    [SerializeField] private float finalPhysDEF;
+    [SerializeField] private float finalMagicDEF;
+    [SerializeField] private float finalCritChance;
+    [SerializeField] private float finalCritMult;
+    [SerializeField] private float finalMoveSpeed;
+    [SerializeField] private float finalCoolDownReduce;
+    [SerializeField] private float finalGoldGain;
+    [SerializeField] private float finalExpGain;
+    [SerializeField] private float finalBossDamage;
+    [SerializeField] private float finalNormalDamage;
+    [SerializeField] private float finalDamageMult;
+
     IEnumerator Start()
     {
         yield return new WaitUntil(()=>DataManager.Instance!=null);
         yield return new WaitUntil(() => DataManager.Instance.DataLoad);
         LoadTable();
-
+        testSaveData = JSONService.Load<TestSavePlayerEquipmentData>();
+        testSaveData.InitPlayerEquipmentData();
+        //불러온 데이터 플레이어 장착 및 데이터 세팅
+        equipmentHandler.SetMyEquipOnStart(testSaveData.weaponId, testSaveData.helmetId, testSaveData.gloveId, testSaveData.armorId, testSaveData.bootsId, testSaveData.unlockEquipmentDict);
     }
     public CharacterBaseStat BaseStat { get { return baseStat; } }
 
@@ -72,23 +96,23 @@ public class CharacterStatManager : MonoBehaviour
     public PlayerTrait CoolDownTrait { get { return coolDownTrait; } }
     public PlayerTrait DamageMultTrait { get { return damageMultTrait; } }
 
-    public float FinalHP { get; private set; }
-    public float FinalHPRegen { get; private set; }
-    public float FinalMP { get; private set; }
-    public float FinalMPRegen { get; private set; }
-    public float FinalATK { get; private set; }
-    public float FinalATKSpeed { get; private set; }
-    public float FinalPhysDEF { get; private set; }
-    public float FinalMagicDEF { get; private set; }
-    public float FinalCritChance { get; private set; }
-    public float FinalCritMult { get; private set; }
-    public float FinalMoveSpeed { get; private set; }
-    public float FinalCoolDownReduce { get; private set; }
-    public float FinalGoldGain { get; private set; }
-    public float FinalExpGain { get; private set; }
-    public float FinalBossDamage { get; private set; }
-    public float FinalNormalDamage { get; private set; }
-    public float FinalDamageMult { get; private set; }
+    public float FinalHP { get { return finalHP; } }
+    public float FinalHPRegen { get { return finalHPRegen; } }
+    public float FinalMP { get { return finalMP; } }
+    public float FinalMPRegen {  get { return finalMPRegen; } }
+    public float FinalATK {get { return finalATK; } }
+    public float FinalATKSpeed {  get { return finalATKSpeed; } }
+    public float FinalPhysDEF {  get { return finalPhysDEF; } }
+    public float FinalMagicDEF {  get { return finalMagicDEF; } }
+    public float FinalCritChance {  get { return finalCritChance; } }
+    public float FinalCritMult {  get { return finalCritMult; } }
+    public float FinalMoveSpeed {  get { return finalMoveSpeed; } }
+    public float FinalCoolDownReduce {  get { return finalCoolDownReduce; } }
+    public float FinalGoldGain {  get { return finalGoldGain; } }
+    public float FinalExpGain {  get { return finalExpGain; } }
+    public float FinalBossDamage {  get { return finalBossDamage; } }
+    public float FinalNormalDamage {  get { return finalNormalDamage; } }
+    public float FinalDamageMult {  get { return finalDamageMult; } }
 
     public event Action<PlayerStatType, float> StatUpdate;
 
@@ -157,29 +181,29 @@ public class CharacterStatManager : MonoBehaviour
 
     public void FinalStat()
     {
-        FinalHP = FinalStatAdd(PlayerStatType.HP,baseStat.HP, hpStatUpgrade.Stat, levelBonus.BonusHP, hpTrait.CurrentLevel, hpTrait.StatUP);
-        FinalHPRegen = FinalStatAdd(PlayerStatType.HP_REGEN,baseStat.HpRegeneration, hpRegenStatUpgrade.Stat, levelBonus.BonusHPRegen, 0, 0);
-        FinalMP = FinalStatAdd(PlayerStatType.MP,baseStat.Mana, mpStatUpgrade.Stat, levelBonus.BonusMP, mpTrait.CurrentLevel, mpTrait.StatUP);
-        FinalMPRegen = FinalStatAdd(PlayerStatType.MP_REGEN, baseStat.ManaRegeneration, mpRegenStatUpgrade.Stat, levelBonus.BonusMPRegen, 0, 0);
-        FinalATK = FinalStatAdd(PlayerStatType.ATK, baseStat.Attack, attackStatUpgrade.Stat, levelBonus.BonusAttack, attackTrait.CurrentLevel, attackTrait.StatUP);
-        FinalATKSpeed = FinalStatAdd(PlayerStatType.ATK_SPEED, baseStat.AttackSpeed, 0, 0, attackSpeedTrait.CurrentLevel, attackSpeedTrait.StatUP);
-        FinalPhysDEF = FinalStatAdd(PlayerStatType.PHYS_DEF, baseStat.PhysicsResist, 0, 0, 0, 0);
-        FinalMagicDEF = FinalStatAdd(PlayerStatType.MAGIC_DEF, baseStat.MagicResist, 0, 0, 0, 0);
-        FinalCritChance = FinalStatAdd(PlayerStatType.CRIT_CHANCE, baseStat.CriticalChance, critStatUpgrade.Stat, 0, critTrait.CurrentLevel, critTrait.StatUP);
-        FinalCritMult = FinalStatAdd(PlayerStatType.CRIT_MULT, baseStat.CriticalMultiplier, critMultStatUpgrade.Stat, levelBonus.BonusCriticalDamage, critMultTrait.CurrentLevel, critMultTrait.StatUP);
-        FinalMoveSpeed = FinalStatAdd(PlayerStatType.MOVE_SPEED, baseStat.MoveSpeed, 0, 0, 0, 0);
-        FinalCoolDownReduce = FinalStatAdd(PlayerStatType.COOLDOWN_REDUCE, baseStat.CoolDown, 0, 0, coolDownTrait.CurrentLevel, coolDownTrait.StatUP);
-        FinalGoldGain = FinalStatAdd(PlayerStatType.GOLD_GAIN, baseStat.GoldGain, 0, 0, 0, 0);
-        FinalExpGain = FinalStatAdd(PlayerStatType.EXP_GAIN, baseStat.ExpGain, 0, 0, 0, 0);
-        FinalBossDamage = FinalStatAdd(PlayerStatType.BOSS_DMG, baseStat.BossDamage, bossDamageStatUpgrade.Stat, 0, bossDamageTrait.CurrentLevel, bossDamageTrait.StatUP);
-        FinalNormalDamage = FinalStatAdd(PlayerStatType.NORMAL_DMG, baseStat.NormalDamage, 0, 0, 0, 0);
-        FinalDamageMult = FinalStatAdd(PlayerStatType.DMG_MULT, baseStat.DamageMult, 0, 0, 0, 0);
+        finalHP = FinalStatAdd(PlayerStatType.HP,baseStat.HP, hpStatUpgrade.Stat, levelBonus.BonusHP, hpTrait.CurrentLevel, hpTrait.StatUP);
+        finalHPRegen = FinalStatAdd(PlayerStatType.HP_REGEN,baseStat.HpRegeneration, hpRegenStatUpgrade.Stat, levelBonus.BonusHPRegen, 0, 0);
+        finalMP = FinalStatAdd(PlayerStatType.MP,baseStat.Mana, mpStatUpgrade.Stat, levelBonus.BonusMP, mpTrait.CurrentLevel, mpTrait.StatUP);
+        finalMPRegen = FinalStatAdd(PlayerStatType.MP_REGEN, baseStat.ManaRegeneration, mpRegenStatUpgrade.Stat, levelBonus.BonusMPRegen, 0, 0);
+        finalATK = FinalStatAdd(PlayerStatType.ATK, baseStat.Attack, attackStatUpgrade.Stat, levelBonus.BonusAttack, attackTrait.CurrentLevel, attackTrait.StatUP);
+        finalATKSpeed = FinalStatAdd(PlayerStatType.ATK_SPEED, baseStat.AttackSpeed, 0, 0, attackSpeedTrait.CurrentLevel, attackSpeedTrait.StatUP);
+        finalPhysDEF = FinalStatAdd(PlayerStatType.PHYS_DEF, baseStat.PhysicsResist, 0, 0, 0, 0);
+        finalMagicDEF = FinalStatAdd(PlayerStatType.MAGIC_DEF, baseStat.MagicResist, 0, 0, 0, 0);
+        finalCritChance = FinalStatAdd(PlayerStatType.CRIT_CHANCE, baseStat.CriticalChance, critStatUpgrade.Stat, 0, critTrait.CurrentLevel, critTrait.StatUP);
+        finalCritMult = FinalStatAdd(PlayerStatType.CRIT_MULT, baseStat.CriticalMultiplier, critMultStatUpgrade.Stat, levelBonus.BonusCriticalDamage, critMultTrait.CurrentLevel, critMultTrait.StatUP);
+        finalMoveSpeed = FinalStatAdd(PlayerStatType.MOVE_SPEED, baseStat.MoveSpeed, 0, 0, 0, 0);
+        finalCoolDownReduce = FinalStatAdd(PlayerStatType.COOLDOWN_REDUCE, baseStat.CoolDown, 0, 0, coolDownTrait.CurrentLevel, coolDownTrait.StatUP);
+        finalGoldGain = FinalStatAdd(PlayerStatType.GOLD_GAIN, baseStat.GoldGain, 0, 0, 0, 0);
+        finalExpGain = FinalStatAdd(PlayerStatType.EXP_GAIN, baseStat.ExpGain, 0, 0, 0, 0);
+        finalBossDamage = FinalStatAdd(PlayerStatType.BOSS_DMG, baseStat.BossDamage, bossDamageStatUpgrade.Stat, 0, bossDamageTrait.CurrentLevel, bossDamageTrait.StatUP);
+        finalNormalDamage = FinalStatAdd(PlayerStatType.NORMAL_DMG, baseStat.NormalDamage, 0, 0, 0, 0);
+        finalDamageMult = FinalStatAdd(PlayerStatType.DMG_MULT, baseStat.DamageMult, 0, 0, 0, 0);
     }
 
     private float FinalStatAdd(PlayerStatType playerStatType ,float baseStat, float upgradeStat, float levelHP, int traitLevel, float traitStat)
     {
-
-        float complate = baseStat + upgradeStat + levelHP + (traitLevel * traitStat);
+        float equipStat = playerSlot.GetStat(playerStatType);
+        float complate = baseStat + upgradeStat + levelHP + (traitLevel * traitStat) + equipStat;
 
         StatUpdate?.Invoke(playerStatType, complate);
 
@@ -196,6 +220,7 @@ public class CharacterStatManager : MonoBehaviour
         playerTrait.Upgrade(ref traitManager.StatPoints);
         TraitUpdate?.Invoke(playerTrait.ID, playerTrait.CurrentLevel, playerTrait.MaxLevel);
     }
+
 
     public void EventSet()
     {
@@ -219,9 +244,11 @@ public class CharacterStatManager : MonoBehaviour
         coolDownTrait.UpgradeTrait += FinalStat;
         damageMultTrait.UpgradeTrait += FinalStat;
     }
-
-    private void FailLoadTable(TableBase table)
+    protected override void OnApplicationQuit()
     {
-        Debug.Log($"[CharacterStatManager] [{table.GetType().ToString()}] 테이블 불러오기 실패");
+        base.OnApplicationQuit();
+        PlayerEquipment playerEquipment = equipmentHandler.playerEquipment;
+        testSaveData.SaveBeforeQuit(playerEquipment.weapon.ID, playerEquipment.helmet.ID, playerEquipment.glove.ID, playerEquipment.armor.ID, playerEquipment.boots.ID);
+        JSONService.Save(testSaveData);
     }
 }
