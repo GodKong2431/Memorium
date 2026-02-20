3b742afdc0e82071e6084f217428ae0895084d3a
GodKong2431 <kizna10231@gmail.com>
2026-02-20 09:36:32 +0900

    [Fix] 테이블 수정

--- Assets/00. Scripts/Data/CSV/Class/Enemy/MonsterGroupTable.cs
+++ Assets/00. Scripts/Data/CSV/Class/Enemy/MonsterGroupTable.cs
@@ -4,8 +4,10 @@ using System.Collections.Generic;
 [System.Serializable]
 public class MonsterGroupTable : TableBase
 {
+    public int monsterSpawnGroup;
     public SpawnerType spawnerType;
     public string spawnerName;
     public MonsterType monsterType;
     public int MonsterID;
+    public int monsterSpawnCount;
 }
