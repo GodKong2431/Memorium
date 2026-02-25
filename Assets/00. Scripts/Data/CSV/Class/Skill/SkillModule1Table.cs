1e0c58c55b5d4c6facb8d8892fc7e091ab129ca3
GodKong2431 <kizna10231@gmail.com>
2026-02-25 10:57:51 +0900

    [Add] 오류없는 CSV 추가

---
+++ Assets/00. Scripts/Data/CSV/Class/Skill/SkillModule1Table.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class SkillModule1Table : TableBase
+{
+    public M1Type m1Type;
+    public float m1Delay;
+    public float m1Scale;
+    public float m1Duration;
+    public string desc;
+}
---
+++ Assets/00. Scripts/Data/CSV/Class/Skill/SkillModule3Table.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class SkillModule3Table : TableBase
+{
+    public M3Type m3Type;
+    public float m3Delay;
+    public float m3Scale;
+    public float m3Distance;
+    public float m3Duration;
+    public float m3TickInterval;
+    public string desc;
+}
