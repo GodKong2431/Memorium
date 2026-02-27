23546f0e2758db73683ffe498c9444abac5e56cb
KHY-dev <dktl0skdy@gmail.com>
2026-02-27 10:04:44 +0900

    [Fix] CurrenyType에 임시로 추가했던 SkillScroll 제거

--- Assets/00. Scripts/Enemy/Reward/ItemDropLogic.cs
+++ Assets/00. Scripts/Enemy/Reward/ItemDropLogic.cs
@@ -1,4 +1,4 @@
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
@@ -14,7 +14,7 @@ public static class ItemDropLogic
     {
         { ItemCategory.PixieFragment, CurrencyType.PixieFragment },
         { ItemCategory.DungeonTicket, CurrencyType.DungeonTicket },
-        { ItemCategory.SkillScroll, CurrencyType.SkillScroll },
+        //{ ItemCategory.SkillScroll, CurrencyType.SkillScroll },
     };
 
     public static bool TryGetCurrencyType(ItemCategory category, out CurrencyType currencyType) =>
