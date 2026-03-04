a3844b05bb837b858936a2491adfe082bb2fb667
KHY-dev <dktl0skdy@gmail.com>
2026-03-04 18:26:52 +0900

    [Add] CurrentyType에 장비 뽑기권 추가

--- Assets/00. Scripts/Manager/CurrencyType.cs
+++ Assets/00. Scripts/Manager/CurrencyType.cs
@@ -1,4 +1,4 @@
-﻿
+
 
 public enum CurrencyType
 {
@@ -8,5 +8,8 @@ public enum CurrencyType
     Gold=81,
     Crystal=82,
     DungeonTicket=83,
+    // 가챠 재화 (김혜영: 딱히 define 내용 없어서 숫자 지정하지 않았습니다)
+    WeaponDrawTicket,
+    ArmorDrawTicket,
     //Count = 6,//초기화 및 반복문에서 사용하기 위한 개수 체크용
 }
