330cc3f9dd427adfcd2fc297bd5b9a8e56d10922
Yanghyeonyong <didgusdyd2@gmail.com>
2026-03-05 11:07:35 +0900

    [Add] 퀘스트 데이터 저장 기능 구현

--- Assets/00. Scripts/Data/Json/SaveQuestData.cs
+++ Assets/00. Scripts/Data/Json/SaveQuestData.cs
@@ -4,4 +4,37 @@ using UnityEngine;
 public class SaveQuestData
 {
     //현재 진행 중인 퀘스트 및 진행도를 저장하는 cs
+    public int currentQuestId = -1;
+    public int currentProgress = 0;
+
+    public SaveQuestData()
+    { }
+
+    public (int id, int progress) InitQuestData()
+    {
+        return (ReturnQuestId(), ReturnProgress());
+    }
+    public int ReturnQuestId()
+    {
+        if (currentQuestId <= 0)
+        {
+            return 7010001;
+        }
+        return currentQuestId;
+    }
+    public int ReturnProgress()
+    {
+        return currentProgress;
+    }
+    public void SaveID(int id)
+    { currentQuestId = id; }
+
+    public void SaveProgress(int progress)
+    { currentProgress = progress; }
+
+    public void Save(int id, int progress)
+    {
+        currentQuestId = id;
+        currentProgress = progress;
+    }
 }
