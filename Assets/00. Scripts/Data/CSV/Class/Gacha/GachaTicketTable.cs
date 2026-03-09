3ece1c2e1ffdb3da877ec46e4f3ca47c27b16faa
KHY-dev <dktl0skdy@gmail.com>
2026-03-09 17:41:00 +0900

    [Fix] 뽑기권 타입 > 가챠 타입으로 통일

--- Assets/00. Scripts/Data/CSV/Class/Gacha/GachaTicketTable.cs
+++ Assets/00. Scripts/Data/CSV/Class/Gacha/GachaTicketTable.cs
@@ -1,10 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
 [System.Serializable]
 public class GachaTicketTable : TableBase
 {
-    public TicketType ticketType;
+    public GachaType ticketType;
     public int price;
     public string ticketResources;
     public string ticketName;
