166673db2026bd06014efd0d5bdbbe6e22193ffc
ForestPool <zxpqzx123456@gmail.com>
2026-03-16 11:40:35 +0900

    [Add] 어빌리티 스톤 아이콘 SO

---
+++ Assets/00. Scripts/UI/AbilityStoneUI/StoneIconSO.cs
@@ -0,0 +1,26 @@
+using AYellowpaper.SerializedCollections;
+using UnityEngine;
+
+public enum AblityStoneEnum
+{
+    None,
+    Success,
+    Fail,
+    Decrease,
+}
+
+[System.Serializable]
+public struct AblityStoneEnforce
+{
+    public Sprite enforceSprite;
+    public Color myColor;
+    
+}
+
+[CreateAssetMenu(fileName = "StoneIconSO", menuName = "Scriptable Objects/StoneIconSO")]
+public class StoneIconSO : ScriptableObject
+{
+    [SerializeField] public SerializedDictionary<AblityStoneEnum, AblityStoneEnforce> EnforceSprites = new SerializedDictionary<AblityStoneEnum, AblityStoneEnforce>();
+    
+    [SerializeField] public Sprite LockSprite;
+}
