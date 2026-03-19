bfa111b17ea497ab77c109b23c6597c617ae6d7f
Yanghyeonyong <didgusdyd2@gmail.com>
2026-03-19 16:37:51 +0900

    [Fix] 플레이어 부활 기능 수정

--- Assets/00. Scripts/Player/PlayerStateMachine/PlayerStateDie.cs
+++ Assets/00. Scripts/Player/PlayerStateMachine/PlayerStateDie.cs
@@ -1,4 +1,4 @@
-﻿using NUnit.Framework;
+using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -16,7 +16,6 @@ public class PlayerStateDie : IPlayerState
 
     public void OnExit(PlayerStateContext ctx)
     {
-        ctx.Heal(ctx.MaxHealth);
     }
 
     public void OnUpdate(PlayerStateContext ctx)
