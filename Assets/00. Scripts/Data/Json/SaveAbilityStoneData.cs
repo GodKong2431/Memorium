2924b504535149ccedc41c4b8914ee92332bc624
ForestPool <zxpqzx123456@gmail.com>
2026-03-20 12:59:49 +0900

    [Fix] 어빌리티 스톤 티어 추가 후 저장방식 변경

--- Assets/00. Scripts/Data/Json/SaveAbilityStoneData.cs
+++ Assets/00. Scripts/Data/Json/SaveAbilityStoneData.cs
@@ -1,4 +1,3 @@
-using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using UnityEngine;
@@ -20,30 +19,39 @@ public class SaveAbilityStoneData:ISaveData
 
     public void InitAblityStoneData()
     {
+        AbilityStoneSO so = AbilityStoneManager.Instance != null ? AbilityStoneManager.Instance.so : null;
+
         if (!onCBT)
         {
             abiltyStoneDictDatas = null;
             onCBT = true;
         }
 
-
-        if (abiltyStoneDictDatas == null||abiltyStoneDictDatas.Count <= 0)
+        if (abiltyStoneDictDatas == null)
         {
-            abiltyStoneDictDatas=new List<AbiltyStoneDictData>();
-            foreach (StoneGrade grade in Enum.GetValues(typeof(StoneGrade)))
-            {
-                abiltyStoneDictDatas.Add(new AbiltyStoneDictData(grade));
-            }
+            abiltyStoneDictDatas = new List<AbiltyStoneDictData>();
         }
+
+        EnsureTierGradeEntries(so);
     }
 
     public void LoadAbilityStoneData(AbilityStoneSO so)
     {
         if (abiltyStoneDictDatas == null || abiltyStoneDictDatas.Count <= 0)
+        {
             InitAblityStoneData();
+        }
+
+        EnsureTierGradeEntries(so);
+
         foreach (AbiltyStoneDictData stoneDictData in abiltyStoneDictDatas)
         {
-            if (!so.AbilityStoneDict.TryGetValue((StoneGrade)stoneDictData.stoneGrade, out AbilityStone stone)
+            if (so == null || !so.AbilityStoneDict.TryGetValue(stoneDictData.tier, out var tierStoneDict))
+            {
+                continue;
+            }
+
+            if (!tierStoneDict.TryGetValue((StoneGrade)stoneDictData.stoneGrade, out AbilityStone stone)
                 || stone == null)
             {
                 continue;
@@ -66,45 +74,94 @@ public class SaveAbilityStoneData:ISaveData
 
     public void SaveAbilityStoneDataBySO(AbilityStoneSO so)
     {
-        foreach (AbilityStone abilityStone in so.AbilityStoneDict.Values)
+        if (so == null)
         {
-            //해당 스톤 그레이드의 인덱스를 찾아라
-            int index = abiltyStoneDictDatas.FindIndex
-                (x => x.stoneGrade == (int)abilityStone.StoneGrade);
-            if (index == -1)
-            {
-                abiltyStoneDictDatas.Add(new AbiltyStoneDictData(abilityStone.StoneGrade));
-                index = abiltyStoneDictDatas.Count - 1;
-                Debug.Log("[SaveAbilityStoneData]  새로 만든다");
-            }
-
-            AbiltyStoneDictData stoneDictData = abiltyStoneDictDatas[index];
-            AbilityStoneData stoneData = stoneDictData.abilityStoneData;
-            stoneData.isUnlock = abilityStone.isUnlock;
-
-           
-            if (stoneData.slots == null)
-            {
-                Debug.Log("[SaveAbilityStoneData] 슬롯이 없다");
-            }
-            for (int i = 0; i < stoneData.slots.Count; i++)
-            {
-                AbilityStoneSlotData slotData = stoneData.slots[i];
-                slotData.statType = (int)abilityStone.Slots[i].SlotType;
-                slotData.successCounter = abilityStone.Slots[i].successCounter;
-                stoneData.slots[i]=slotData;
-            }
-            stoneDictData.abilityStoneData = stoneData;
-
-            abiltyStoneDictDatas[index] = stoneDictData;
+            return;
         }
 
-        isDirty= true;
+        if (abiltyStoneDictDatas == null)
+        {
+            abiltyStoneDictDatas = new List<AbiltyStoneDictData>();
+        }
+
+        EnsureTierGradeEntries(so);
+
+        foreach (var tierEntry in so.AbilityStoneDict)
+        {
+            int tierKey = tierEntry.Key;
+            foreach (AbilityStone abilityStone in tierEntry.Value.Values)
+            {
+                if (abilityStone == null)
+                {
+                    continue;
+                }
+
+                //해당 스톤 그레이드의 인덱스를 찾아라
+                int index = abiltyStoneDictDatas.FindIndex
+                    (x => x.tier == tierKey && x.stoneGrade == (int)abilityStone.StoneGrade);
+                if (index == -1)
+                {
+                    abiltyStoneDictDatas.Add(new AbiltyStoneDictData(abilityStone.StoneGrade, tierKey));
+                    index = abiltyStoneDictDatas.Count - 1;
+                    Debug.Log("[SaveAbilityStoneData]  새로 만든다");
+                }
+
+                AbiltyStoneDictData stoneDictData = abiltyStoneDictDatas[index];
+                AbilityStoneData stoneData = stoneDictData.abilityStoneData;
+                stoneData.isUnlock = abilityStone.isUnlock;
+
+
+                if (stoneData.slots == null || stoneData.slots.Count != abilityStone.Slots.Count)
+                {
+                    stoneData = new AbilityStoneData(stoneData.isUnlock, null);
+                }
+
+                int slotCount = Mathf.Min(stoneData.slots.Count, abilityStone.Slots.Count);
+                for (int i = 0; i < slotCount; i++)
+                {
+                    AbilityStoneSlotData slotData = stoneData.slots[i];
+                    slotData.statType = (int)abilityStone.Slots[i].SlotType;
+                    slotData.successCounter = abilityStone.Slots[i].successCounter;
+                    stoneData.slots[i] = slotData;
+                }
+                stoneDictData.abilityStoneData = stoneData;
+
+                abiltyStoneDictDatas[index] = stoneDictData;
+            }
+        }
+        isDirty = true;
     }
 
 
     public void ClearDirty()
     {
         isDirty = false;
+    }
+
+    private void EnsureTierGradeEntries(AbilityStoneSO so)
+    {
+        if (so == null || so.AbilityStoneDict == null)
+        {
+            return;
+        }
+
+        if (abiltyStoneDictDatas == null)
+        {
+            abiltyStoneDictDatas = new List<AbiltyStoneDictData>();
+        }
+
+        foreach (int tierKey in so.AbilityStoneDict.Keys)
+        {
+            foreach (StoneGrade grade in Enum.GetValues(typeof(StoneGrade)))
+            {
+                int index = abiltyStoneDictDatas.FindIndex(
+                    x => x.tier == tierKey && x.stoneGrade == (int)grade);
+
+                if (index == -1)
+                {
+                    abiltyStoneDictDatas.Add(new AbiltyStoneDictData(grade, tierKey));
+                }
+            }
+        }
     }
 }
